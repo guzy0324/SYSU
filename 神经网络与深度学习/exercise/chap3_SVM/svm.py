2dc412503ad6dc996a6cba85644649022c329f9f
Taco <1141374791@qq.com>
2021-10-08 21:20:31 +0800

    CNN

--- 神经网络与深度学习/exercise/chap3_SVM/svm.py
+++ 神经网络与深度学习/exercise/chap3_SVM/svm.py
@@ -2,6 +2,25 @@
 # encoding: utf-8
 
 import numpy as np
+import torch
+from torch import nn
+from tqdm import tqdm
+from torch import optim
+
+Elinear = nn.MSELoss(reduction='sum')
+
+Elogistic = nn.SoftMarginLoss(reduction='sum')
+
+
+def ESVM(y, t):
+    return (1 - y * t).clamp(min=0).sum()
+
+
+epoch = 10000
+lr = 1e-6
+Lambda = 1e-2
+criterion = ESVM
+freq = 1000
 
 
 def load_data(fname):
@@ -31,10 +50,12 @@ class SVM():
     """
     SVM模型。
     """
-
     def __init__(self):
         # 请补全此处代码
-        pass
+        self.linear = nn.Linear(2, 1)
+        self.optimizer = optim.SGD(self.linear.parameters(),
+                                   lr=lr,
+                                   weight_decay=Lambda)
 
     def train(self, data_train):
         """
@@ -42,6 +63,22 @@ class SVM():
         """
 
         # 请补全此处代码
+        x1, x2, t = zip(*data_train)
+        x = torch.cat((torch.tensor(x1, dtype=torch.float32).reshape(
+            (-1, 1)), torch.tensor(x2, dtype=torch.float32).reshape((-1, 1))),
+                      dim=1)
+        self.x_mean = x.mean()
+        self.x_std = x.std()
+        x = (x - self.x_mean) / self.x_std
+        t = torch.tensor(t, dtype=torch.float32).reshape(-1, 1)
+        for i in tqdm(range(epoch)):
+            self.optimizer.zero_grad()
+            y = self.linear(x)
+            loss = criterion(y, t)
+            loss.backward()
+            self.optimizer.step()
+            if i % freq == 0:
+                print(loss.item())
 
     def predict(self, x):
         """
@@ -49,6 +86,9 @@ class SVM():
         """
 
         # 请补全此处代码
+        return torch.where(
+            self.linear((torch.tensor(x, dtype=torch.float32) - self.x_mean) /
+                        self.x_std) > 0, 1., -1.).reshape(-1).numpy()
 
 
 if __name__ == '__main__':
@@ -59,6 +99,7 @@ if __name__ == '__main__':
     data_test = load_data(test_file)
 
     # 使用训练集训练SVM模型
+    torch.manual_seed(0)
     svm = SVM()  # 初始化模型
     svm.train(data_train)  # 训练模型
 
